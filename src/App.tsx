9043c1e33644cb8ef40409a216f201ddb6479439
Alexander King <alexhking@gmail.com>
2023-10-26 16:09:30 -0400

    New logo. Addition of svg plugin. Adjustments to waveform styling.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@
 import React from "react";
 import "./App.css";
 import Grid from "./Grid";
+import LoopLogo from '../src/assets/loop-logo.svg?react'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Reflect Play Alpha</h1>
+      <LoopLogo
+        className="loopLogo"
+      />
       <Grid />
     </div>
   );
